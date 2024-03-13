09847158fff8ed56f6da02b87b9b37bb17fa9898
Francisco Blanco <blancofrancisco34@gmail.com>
2024-03-12 19:20:27 -0500

    flb

--- cjs/CSC/index.d.ts
+++ cjs/CSC/index.d.ts
@@ -13,6 +13,8 @@ export interface CityProps {
     id: number;
     id_state: number;
     text: string;
+    id_country?: number;
+    nameAve?: string | undefined;
 }
 export interface CSCProps {
     country?: CountryProps;
