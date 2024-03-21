0cb6de3e75db6533a97e455b37e99132a5fa5175
Francisco Blanco <blancofrancisco34@gmail.com>
2024-03-21 11:23:28 -0500

    add lant and lng

--- src/AddressGoogle/index.tsx
+++ src/AddressGoogle/index.tsx
@@ -116,6 +116,8 @@ export interface AddressGoogle {
     adr_address?: string;
     aspects?: PlaceAspectRating[];
     business_status?: BusinessStatus;
+    lat?:number
+    lng?:number
     formatted_address?: string;
     formatted_phone_number?: string;
     geometry?: PlaceGeometry;
